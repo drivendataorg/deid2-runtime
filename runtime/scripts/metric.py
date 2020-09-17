1ebe14aced1596b1af9ae05bddf92717f9645522
Isaac Slavitt <isaac.slavitt@gmail.com>
2020-09-17 12:40:09 -0400

    Add project-specific files

---
+++ runtime/scripts/metric.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy.spatial.distance import jensenshannon
+
+
+class Deid2Metric:
+    def __init__(self):
+        self.threshold = 0.05
+        self.misleading_presence_penalty = 0.2
+        self.bias_penalty = 0.25
+        self.allowable_raw_bias = 500
+        self.epsilon = 1e-9
+
+    @staticmethod
+    def _normalize(arr):
+        """ Turn raw counts into frequencies but handle all-zero arrays gracefully """
+        if (arr > 0).any():
+            return arr / arr.sum()
+        return arr
+
+    def _zero_below_threshold(self, arr):
+        """ Take any entries that are below the threshold we care about and zero them out """
+        return np.where(self._normalize(arr) >= self.threshold, arr, 0)
+
+    def _score_counts(self, actual, predicted):
+        """ Score one row of counts for a particular (neighborhood, year, month) """
+        if (actual == predicted).all():
+            return 0.0, 0.0, 0.0
+
+        # zero out entries below the threshold
+        gt = self._zero_below_threshold(actual).ravel()
+        dp = self._zero_below_threshold(predicted).ravel()
+
+        # get the base Jensen Shannon distance from the normalized values plus a tiny
+        # value to avoid divide-by-zero errors
+        jsd = jensenshannon(self._normalize(gt + 1e-9), self._normalize(dp + 1e-9))
+
+        # get the overall penalty for hallucinating counts
+        misleading_presence_mask = (gt == 0) & (dp > 0)
+        misleading_presence_penalty = (
+            misleading_presence_mask.sum() * self.misleading_presence_penalty
+        )
+
+        # get the overall penalty for bias
+        bias_mask = np.abs(actual - predicted).sum() > self.allowable_raw_bias
+        bias_penalty = self.bias_penalty if bias_mask.any() else 0
+        return jsd, misleading_presence_penalty, bias_penalty
+
+    def _score_all(self, actual, predicted):
+        n_perms, _ = actual.shape
+        scores = np.zeros(n_perms, dtype=np.float)
+        for i in range(n_perms):
+            scores[i] = np.sum(self._score_counts(actual[i, :], predicted[i, :]))
+        return scores
+
+    def score(self, actual, predicted, return_individual_scores=False):
+        # make sure the submitted values are proper
+        assert np.isfinite(predicted).all()
+        assert (predicted >= 0).all()
+        # get all of the individual scores
+        raw_scores = self._score_all(actual, predicted)
+        # clip all the scores to [0, 1]
+        scores = np.clip(raw_scores, a_min=0.0, a_max=1.0)
+        # sum them up and multiply by 1000
+        overall_score = np.sum(scores) * 1000
+
+        # in some uses (like visualization), it's useful to get the individual scores out too
+        if return_individual_scores:
+            return overall_score, scores
+
+        return overall_score
---
+++ runtime/scripts/score.py
@@ -0,0 +1,52 @@
+import json
+from pathlib import Path
+
+from loguru import logger
+import pandas as pd
+import pytest
+import typer
+
+import metric
+
+INDEX_COLS = ["epsilon", "neighborhood", "year", "month"]
+
+
+def main(
+    submission: Path, ground_truth: Path, json_report: bool = False,
+):
+    logger.info(f"reading submission from {submission} ...")
+    submission_format = pd.read_csv(submission, index_col=INDEX_COLS)
+    logger.info(f"read dataframe with {len(submission_format):,} rows")
+
+    logger.info("validating the submission")
+    pytest.main(["-x", ".", "--submission-file", "submission.csv"])
+
+    logger.info(f"reading ground truth from {ground_truth} ...")
+    ground_truth = pd.read_csv(ground_truth, index_col=INDEX_COLS)
+    logger.info(f"read dataframe with {len(ground_truth):,} rows")
+
+    scorer = metric.Deid2Metric()
+    overall_score, row_scores = scorer.score(
+        ground_truth.values, submission_format.values, return_individual_scores=True
+    )
+    logger.success(f"OVERALL SCORE: {overall_score}")
+
+    if json_report:
+        row_outcomes = []
+        for idx, score in zip(submission_format.index, row_scores):
+            epsilon, neighborhood, year, month = idx
+            row_outcomes.append(
+                {
+                    "epsilon": epsilon,
+                    "neighborhood": neighborhood,
+                    "year": year,
+                    "month": month,
+                    "score": score,
+                }
+            )
+        result = {"score": overall_score, "details": row_outcomes}
+        typer.echo(json.dumps(result, indent=2))
+
+
+if __name__ == "__main__":
+    typer.run(main)
