59bbbd4f41778f6d8f0de0d41734fbbeec8faef8
Isaac Slavitt <isaac.slavitt@gmail.com>
2020-09-22 16:14:48 -0400

    Allow create visualization to take a local template

--- runtime/scripts/create_visualization.py
+++ runtime/scripts/create_visualization.py
@@ -9,7 +9,7 @@ import typer
 JINJA_TEMPLATE_URL = "https://drivendata-competition-deid2-public.s3.amazonaws.com/visualization/report.jinja2"
 
 
-def main(json_report: Path, parameters: Path):
+def main(json_report: Path, parameters: Path, template_path: Path = None):
     logger.info(f"reading report from {json_report} ...")
     json_report = json.loads(json_report.read_text())
 
@@ -18,11 +18,14 @@ def main(json_report: Path, parameters: Path):
     # remove unnecessary clutter we don't use in the visualization
     parameters["schema"].pop("incident_type")
 
-    logger.info(f"downloading template from {JINJA_TEMPLATE_URL}...")
-    r = requests.get(JINJA_TEMPLATE_URL)
-    if not r.status_code == 200:
-        logger.error(f"could not download template! error {r.status_code}")
-    template_text = r.content.decode("utf8")
+    if template_path is None:
+        logger.info(f"downloading template from {JINJA_TEMPLATE_URL}...")
+        r = requests.get(JINJA_TEMPLATE_URL)
+        if not r.status_code == 200:
+            logger.error(f"could not download template! error {r.status_code}")
+        template_text = r.content.decode("utf8")
+    else:
+        template_text = template_path.read_text()
 
     context = {"report": json.dumps(json_report), "parameters": json.dumps(parameters)}
 
