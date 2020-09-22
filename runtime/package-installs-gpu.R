44ca77ea17c9f0fed3abd1dfbbed66a72a20123c
Robert Gibboni <robert@drivendata.org>
2020-09-22 15:21:43 -0400

    Remove r image specific packages

--- runtime/package-installs-gpu.R
+++ runtime/package-installs-gpu.R
@@ -10,12 +10,5 @@ options(repos = r)
 install.packages("remotes", quiet = T)
 library(remotes)
 
-# ============ IMAGE LIBRARIES ============
-install.packages("opencv", quiet = T)
-install.packages("OpenImageR", quiet = T)
-
-remotes::install_github("bnosac/image", subdir = "image.ContourDetector", build_vignettes = F, quiet = T)
-remotes::install_github("bnosac/image", subdir = "image.darknet", build_vignettes = F, quiet = T)
-
 # Keras -- 2.2.4.1 has a preprocessing function incompatibility with current python keras
 remotes::install_github("rstudio/keras", build_vignettes = F, quiet = T)
