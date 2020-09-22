a3aa3ac7c0bf42d02863ffa0c3cce9604a9c4450
Robert Gibboni <robert@drivendata.org>
2020-09-22 16:01:29 -0400

    Remove image R package installs

--- runtime/package-installs-gpu.R
+++ runtime/package-installs-gpu.R
@@ -9,6 +9,3 @@ options(repos = r)
 
 install.packages("remotes", quiet = T)
 library(remotes)
-
-# Keras -- 2.2.4.1 has a preprocessing function incompatibility with current python keras
-remotes::install_github("rstudio/keras", build_vignettes = F, quiet = T)
