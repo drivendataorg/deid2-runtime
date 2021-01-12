c3ab314b7d3559adfb661231d78323c7b951702a
Isaac Slavitt <isaac.slavitt@gmail.com>
2021-01-12 10:25:58 -0500

    Speed up baseline

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,9 +1,9 @@
 import csv
 import json
+import random
 from pathlib import Path
 
 from loguru import logger
-import numpy as np
 import pandas as pd
 from tqdm import trange
 import typer
@@ -17,44 +17,32 @@ DEFAULT_PARAMS = DATA_DIRECTORY / "parameters.json"
 DEFAULT_OUTPUT = ROOT_DIRECTORY / "submission.csv"
 
 
-def simulate_row(parameters, epsilon=None, sim_individual_id=None):
+def simulate_row(parameters):
     """
     Naively create a valid row by picking random but valid values using the parameters file.
     """
     row = {}
-    if epsilon is not None:
-        row["epsilon"] = epsilon
     for col, d in parameters["schema"].items():
         value = 0
         if "values" in d:
-            value = np.random.choice(d["values"])
+            value = random.choice(d["values"])
         elif "min" in d:
-            if "int" in d["dtype"]:
-                value = np.random.randint(d["min"], d["max"])
-            else:
-                value = np.random.uniform(d["min"], d["max"])
-        elif col in {"HHWT", "PERWT"}:
-            value = np.round(np.random.exponential(100), 1)
+            value = random.randint(d["min"], d["max"])
         elif col in {"INCTOT", "INCWAGE", "INCEARN"}:
-            value = int(np.random.exponential(30_000))
+            value = random.randint(0, 60_000)
         row[col] = value
-    if sim_individual_id is not None:
-        row["sim_individual_id"] = sim_individual_id
     return row
 
 
 def main(
-    parameters_file: Path = DEFAULT_PARAMS,
-    ground_truth_file: Path = DEFAULT_GROUND_TRUTH,
-    output_file: Path = DEFAULT_OUTPUT,
-    n_rows_to_simulate_per_epsilon: int = 20_000,
-    random_seed: int = 42,
+        parameters_file: Path = DEFAULT_PARAMS,
+        ground_truth_file: Path = DEFAULT_GROUND_TRUTH,
+        output_file: Path = DEFAULT_OUTPUT,
+        n_rows_to_simulate_per_epsilon: int = 20_000,
 ):
     """
     Create synthetic data appropriate to be submitted to the Sprint 2 competition.
     """
-    np.random.seed(random_seed)
-
     logger.info(f"reading schema from {parameters_file} ...")
     with parameters_file.open("r") as fp:
         parameters = json.load(fp)
@@ -88,7 +76,9 @@ def main(
                 # NOTE: Naively assume only one row per individual (and lazily use iteration   #
                 #       number as the simulated individual ID).                                #
                 ################################################################################
-                row = simulate_row(parameters, epsilon=epsilon, sim_individual_id=i)
+                row = simulate_row(parameters)
+                row["epsilon"] = epsilon
+                row["sim_individual_id"] = i
                 output.writerow(row)
                 n_rows += 1
     logger.success(f"finished writing {n_rows:,} rows to {output_file}")
